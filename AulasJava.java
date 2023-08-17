5c805aea2be68dfa51f8329eccdc5cc94e4dbb84
Gustavo0310 <111024445+Gustavo031001@users.noreply.github.com>
2023-08-17 20:39:35 -0300

    Add files via upload

---
+++ AulasJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulasjava;
+
+/**
+ *
+ * @author Aluno
+ */
+public class AulasJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 10;
+        int valor1 = 30;
+        int soma = valor + valor1;
+        
+        System.out.println("O valor da soma  é: "+soma+" Primeiro Programa");
+    }
+    
+}
